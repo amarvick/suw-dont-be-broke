afa16f94c40e61c172b63c50f61f43aeaffdf159
Alex Marvick <alex@fiduciarytech.com>
2018-10-06 09:44:04 -0700

    Setting up routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import Home from './js/components/home'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Home />
+          <Router>
+            <div>
+              <Route exact path='/' component={Home}/>
+              {/* <Route path='/about' component={About}/> */}
+              {/* <Route path='/services' component={Services}/> */}
+              {/* <Route path='/contact' component={Contact}/> */}
+              {/* <Route path='/blog' component={Blog}/> */}
+              {/* <Route path='/gender' component={Gender}/> */}
+              {/* <Route path='/drones' component={Drones}/> */}
+              {/* <Route path='/software' component={Software}/> */}
+            </div>
+          </Router>
         </header>
       </div>
     );
