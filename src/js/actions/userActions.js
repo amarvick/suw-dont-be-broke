5d2ba1c7898df6bb52bd54385977c49f69fda11e
Alex Marvick <alex@fiduciarytech.com>
2018-10-06 21:15:58 -0700

    WE ARE GOING LOWER CASE!!!!!!

--- src/js/actions/userActions.js
+++ src/js/actions/userActions.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 // import { loadingInProgress, loadingComplete } from './inGameActions'
 
-import UserActionTypes from '../actionTypes/userActionTypes'
+import UserActionTypes from '../actiontypes/userActionTypes'
 
 // AM - for dispatch functions that have type/payload (16/17, 23/24), consider passing these in to function. See line 26, dispatch(loadingComplete())
 
