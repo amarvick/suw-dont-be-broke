15ab83eca1c6d49651fe6d88dafddcec308464d6
Alex Marvick <alex@fiduciarytech.com>
2018-10-07 11:56:35 -0700

    Merging stuff from master in to our branch

--- src/js/components/home/components/eventsCalendar.js
+++ src/js/components/home/components/eventsCalendar.js
@@ -12,7 +12,7 @@ import AppBar from 'material-ui/AppBar'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
 import Calendar from 'react-calendar'
-// import something from ''
+import white from ''
 import '../../../../stylesheets/eventsCalendar.scss'
 
 class EventsCalendar extends Component {
@@ -29,18 +29,42 @@ class EventsCalendar extends Component {
   onChange = date => this.setState({ date })
 
   render(props) { 
+    let style = { color: white }
+    let theFontSize = { fontSize: 16 }
+    
+    let theEvent
+    let theDate = this.state.date.toLocaleDateString()
+    let theTime
+    let theAddress
+    
+    if (theDate == '10/7/2018') { 
+      theEvent = 'Seattle Startup Weekend pitches!'
+      theTime = '3:00PM'
+      theAddress = 'Seattle Public Library'
+    } else if (theDate == '10/23/2018') { 
+      theEvent = 'Kevin O\'Leary in Seattle!'
+      theTime = '10:30AM' 
+      theAddress = 'Marion Oliver McCaw Hall at Seattle Center, Seattle, WA, US'
+    }
+
     return (
       <div className="eventsCalendar">
-        <h3>
+        <h1 style={style}>
             { this.state.date.toLocaleDateString() }
-        </h3>
+        </h1>
         <Calendar 
             onChange = { this.onChange }
             value = { this.state.date }
         />
-        <h3>
+        <h1 style={style}>
             Find Events Near You
-        </h3>
+        </h1>
+        <p style={{...style, ...theFontSize}}>
+          { theEvent } <br/>
+          { theDate } <br/>
+          { theTime } <br/>
+          { theAddress } <br/>
+        </p>
       </div>
     )
   }
