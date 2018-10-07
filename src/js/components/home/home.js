1b654a510f893a06b7e8d9d5ecd1cfe46e661212
GRicha2380 <gregor.richardson@gmail.com>
2018-10-07 14:14:19 -0700

    progress bar polish

--- src/js/components/home/home.js
+++ src/js/components/home/home.js
@@ -37,16 +37,24 @@ class Home extends Component {
   }
 
   handleClick(event){
-    document.querySelector(".modalWindow").classList.add("showModal");
+    document.querySelector("#articleModal").classList.add("showModal");
     
-    document.querySelector(".modalWindow").addEventListener("click", ()=>{
-      document.querySelector(".modalWindow").classList.remove("showModal");
+    document.querySelector("#articleModal").addEventListener("click", ()=>{
+      document.querySelector("#articleModal").classList.remove("showModal");
       let progressWidth = getComputedStyle(document.querySelector(".progressInner")).width.split("px")[0];
       console.log(Number(progressWidth)+50)
       document.querySelector(".progressInner").setAttribute("style", `width:${Number(progressWidth)+50}px`);
       document.querySelector(".progressPercent").innerText = `${(((Number(progressWidth)+10)/1130)*100).toFixed(0)}%`;
     });
-  } 
+  }
+  
+  learningClick(event){
+    document.querySelector("#learningModal").classList.add("showModal");
+    
+    document.querySelector(".modalWindow").addEventListener("click", ()=>{
+      document.querySelector(".modalWindow").classList.remove("showModal");
+    });
+  }
 
   render(props) {
     const { classes } = this.props;
@@ -56,7 +64,22 @@ class Home extends Component {
 
     return (
       <div className="containerMain">
-      <div className="modalWindow">
+      <div className="modalWindow" id="learningModal">
+        <div id="pathProgress">
+        <h2>Your Learning Path Progress</h2>
+          <div className="circle"><img src="./p1.jpg" /></div>
+          <div className="line"></div>
+          <div className="circle"></div>
+          <div className="line"></div>
+          <div className="circle"></div>
+
+          <div className="yourGoal">Your Goal: <span className="goalName">Budgeting</span><span className="editGoal">
+            <a>Edit</a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="modalWindow" id="articleModal">
         <div className="modalInner">
           <div></div>
           <img src="./article2.jpg" />
@@ -69,13 +92,14 @@ class Home extends Component {
           <a>Edit</a>
           </span>
         </div>
+        <div className="learningPath" onClick={(event) => this.learningClick(event)}>View Learning Path...</div>
         <div className="progressBar">
           <div className="progressInner"><span className="progressPercent">50%</span></div>
+          <div className="progressBarMessage">Great Work! 🎉</div>
         </div>
       </div>
-        <Quotes />
         <div id="featuredItems">
-          <h2>Featured Tasks</h2>
+          <h2>Featured Goals</h2>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/tsin6TIle3A" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
           <div className="featuredRight">
             <div className="featuredText">How does credit work?</div>
@@ -86,36 +110,36 @@ class Home extends Component {
           <div id="budgetingLinks" className="textLinkList">
             <h3>Budgeting</h3>
             <ul>
-              <li>Item One</li>
-              <li>Item Two</li>
-              <li>Item Three</li>
+              <li>Article One</li>
+              <li>Article Two</li>
+              <li>Article Three</li>
             </ul>
           </div>
           
           <div id="investingLinks" className="textLinkList">
             <h3>Investing</h3>
             <ul>
-              <li className="listItem" onClick={(event) => this.handleClick(event)}>Item One</li>
-              <li>Item Two</li>
-              <li>Item Three</li>
+              <li className="listItem" onClick={(event) => this.handleClick(event)}>Article One</li>
+              <li>Article Two</li>
+              <li>Article Three</li>
             </ul>
           </div>
           
           <div id="debtLinks" className="textLinkList">
             <h3>Reducing Debt</h3>
             <ul>
-              <li className="listItem" onClick={(event) => this.handleClick(event)}>Item One</li>
-              <li>Item Two</li>
-              <li>Item Three</li>
+              <li className="listItem" onClick={(event) => this.handleClick(event)}>Article One</li>
+              <li>Article Two</li>
+              <li>Article Three</li>
             </ul>
           </div>
 
           <div id="debtLinks" className="textLinkList">
             <h3>Retirement Planning</h3>
             <ul>
-              <li className="listItem" onClick={(event) => this.handleClick(event)}>Item One</li>
-              <li>Item Two</li>
-              <li>Item Three</li>
+              <li className="listItem" onClick={(event) => this.handleClick(event)}>Article One</li>
+              <li>Article Two</li>
+              <li>Article Three</li>
             </ul>
           </div>
 
