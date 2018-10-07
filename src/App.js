9eddf9db8c788acfbc29fd74cb015e130cdad0a7
Alex Marvick <alex@fiduciarytech.com>
2018-10-06 20:19:30 -0700

    Fixed merge issue

--- src/App.js
+++ src/App.js
@@ -7,21 +7,14 @@ import Navbar from './js/components/navbar'
 import Home from './js/components/home/home'
 import About from './js/components/about/about'
 
+import './stylesheets/nav.scss'
+
 // Import actions here
 import { fetchUser } from './js/actions/userActions'
 
 class App extends Component {
   constructor() {
     super()
-    // 'goodResponse' will change based off of the backend and will determine whether to display the login screen or the user's account
-    // will probably want to put this in using redux state
-    var goodResponse
-
-    this.state = {
-      loggedIn: goodResponse ? true : false
-    }
-  }
-
     this.state = {
       loggedIn: false,
       loggedInUserId: ''
@@ -52,53 +45,47 @@ componentDidMount(){
     return (
       <div className="App">
         <header className="App-header">
+          
           <Navbar />
           { user == null &&
+            <div>
+              <Router>
+                  {/*<Route exact path='/' component={Home}/> *}
+                  {/* <Route path='/about' component={About}/> */}
+                  {/* <Route path='/services' component={Services}/> */}
+                  {/* <Route path='/contact' component={Contact}/> */}
+                  {/* <Route path='/blog' component={Blog}/> */}
+                  {/* <Route path='/gender' component={Gender}/> */}
+                  {/* <Route path='/drones' component={Drones}/> */}
+                  {/* <Route path='/software' component={Software}/> */}
+              </Router>
+              <div className="containerNav">
+                <div className="headerRight">
+                  { (this.state.loggedIn) ? 
+                    <li className="userMenu">
+                      <div className="userMenuToggle">User Menu</div>
+                      <ul className="userMenuContent">
+                        <li href="http://apple.com">User Item 1</li>
+                        <li>User Item 2</li>
+                        <li>User Item 3</li>
+                      </ul>
+                    </li> 
+                    :
+                    <li className="signUp"><a href="/signup">Sign Up</a></li> 
+                  }
+                  
+                </div>
 
-          <Router>
-              {/*<Route exact path='/' component={Home}/> *}
-              {/* <Route path='/about' component={About}/> */}
-              {/* <Route path='/services' component={Services}/> */}
-              {/* <Route path='/contact' component={Contact}/> */}
-              {/* <Route path='/blog' component={Blog}/> */}
-              {/* <Route path='/gender' component={Gender}/> */}
-              {/* <Route path='/drones' component={Drones}/> */}
-              {/* <Route path='/software' component={Software}/> */}
-          </Router>
-          <div className="containerNav">
-            <div className="headerRight">
-            {
-                (this.state.loggedIn) ? 
-                  <li className="userMenu">
-                    <div className="userMenuToggle">User Menu</div>
-                    <ul className="userMenuContent">
-                      <li href="http://apple.com">User Item 1</li>
-                      <li>User Item 2</li>
-                      <li>User Item 3</li>
-                    </ul>
-                  </li> 
-                  :
-                  <li className="signUp"><a href="/signup">Sign Up</a></li> 
-              }
-              
-            </div>
+                <div className="headerLeft">
+                  <li className="active"><a href="/">Home</a></li>
+                  <li><a href="#">About Us</a></li>
+                </div>
 
-            <div className="headerLeft">
-              <li className="active"><a href="/">Home</a></li>
-              <li><a href="#">About Us</a></li>
-            </div>
-
-          </div>
-
-          { user != null && 
-            <Router>
-              <div>
-                <Route exact path='/' component={Home}/>
-                <Route path='/about' component={About}/>
               </div>
-            </Router>
+            </div>
           }
         </header>
+
         <div className="subNav">
           <li>Useful Info here</li>
         </div>
@@ -120,4 +107,4 @@ const mapStateToProps = (state) => ({
   user: state.user.user
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
